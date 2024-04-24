234c19d2e914d3f8bd52ff1f1f531512f18a3f85
Frankmss <frank.mss@proton.me>
2024-04-24 11:20:04 +0800

    hostTools中的print_AD_DA.py可以使用，但是实现的不好，应该按照plt_adc.py那样采用thread方式实现

--- hostTools/print_AD_DA.py
+++ hostTools/print_AD_DA.py
@@ -1,8 +1,22 @@
 import socket
 import struct
+import time
+import matplotlib.pyplot as plt
+#import pandas as pd
+
 
 def receive_packet(s):
    buffer = b''  # 初始化数据缓冲区
+   frames_received = 0
+   start_time = time.time()
+   adc_values = []
+   showAdcMeanList = []
+   # create plot
+   plt.ion() # <-- work in "interactive mode"
+   fig, ax = plt.subplots()
+   fig.canvas.set_window_title('Live Chart')
+   ax.set_title("detect velocity adc val")
+   ax.set_ylim(1000, 3000)  # 设置 y 坐标范围
    while True:
        data = s.recv(1024)  # 一次性读取更多数据
        if not data:
@@ -16,13 +30,34 @@ def receive_packet(s):
                buffer = buffer[start_index + 11:]  # 更新缓冲区数据
 
                try:
-                   adc, dac_p, dac_n = struct.unpack('!HLL', packet[1:])
-                   return adc, dac_p, dac_n
+                   adc, dac_p, dac_n = struct.unpack('<HLL', packet[1:])
+                   #print(f"Received Data: ADC={adc}, DAC_P={dac_p}, DAC_N={dac_n}")
+                   adc_values.append(adc)
+                   
+                   frames_received += 1
                except struct.error:
-                   print("not return")
-                   continue
+                   print("Error parsing packet")
+                   break
+           else:
+               #print("Incomplete packet received")
+               break
 
-   return None
+       # 计算每秒钟帧数
+       elapsed_time = time.time() - start_time
+       if elapsed_time >= 1:
+           meanADC = sum(adc_values) / len(adc_values)
+           print(f"Frames received in the last second: {frames_received}, meanADC={meanADC}")
+           frames_received = 0
+           start_time = time.time()
+           showAdcMeanList.append(meanADC)
+           ax.plot(showAdcMeanList, color='r')
+           # show the plot
+           plt.show()
+           plt.pause(0.0001) # <-- sets the current plot until refreshed
+           time.sleep(.1)
+           adc_values = []
+
+   remaining_data = buffer  # 保留超出11个字节的部分
 
 
 def main():
@@ -36,11 +71,7 @@ def main():
        while True:
            data = receive_packet(s)
 
-           if data:
-               adc, dac_p, dac_n = data
-               print(f"Received Data: ADC={adc}, DAC_P={dac_p}, DAC_N={dac_n}")
-           else:
-               print("Error parsing packet, continuing...")
+
 
 if __name__ == "__main__":
    main()
